9632a58c3adb85efebf5cc526a855a859158023b
nivinpkumar <pkumarnivin@gmail.com>
2021-06-23 16:39:48 +0200

    fixed small bug
    fixed small bug

--- eVotingnode/code/milestone2/auth-verif/authenticate/unique_id.js
+++ eVotingnode/code/milestone2/auth-verif/authenticate/unique_id.js
@@ -2,10 +2,13 @@ const { VerifiableCredential, checkCredential } = require('../node/identity_wasm
 const { createIdentity } = require('./create_did');
 const { manipulateIdentity } = require('./manipulate_did');
 const fs = require("fs");
+const prompt = require("prompt-sync")({ sigint: true });
+
+
 
 function saveQR(qrcode) {
     // saving data in QRcode
-    console.log("Save QRcode >>>>>>>>".green);
+    console.log("Saved verifiable credentials ");
     try {
       fs.writeFileSync("citizen_verifiable_credentials.json", qrcode);
     } catch (e) {
@@ -27,6 +30,7 @@ async function run() {
     console.log(citizen_id);
     console.log("************************issuer*************************");
     console.log(government_id);
+
     // Prepare a credential subject indicating the degree earned by citizen_id
     let credentialSubject = {
         id: citizen_id.doc.id.toString(),
@@ -39,27 +43,62 @@ async function run() {
         gender: "Male",
         bsn: "987452136"
     };
-    // Create an unsigned `UniversityDegree` credential for citizen_id
-    const unsignedVc = VerifiableCredential.extend({
-        id: "http://groninen.edu/credentials/3732",
-        type: "NationalIdentityCredentials",
-        issuer: government_id.doc.id.toString(),
-        credentialSubject,
-    });
-    // Sign the credential with the government_id's newKey
-    const signedVc = government_id.doc.signCredential(unsignedVc, {
-        method: government_id.doc.id.toString()+"#newKey",
-        public: government_id.newKey.public,
-        secret: government_id.newKey.secret,
-    });
+
     console.log("****************final signed credentials*****************");
-    //console.log(signedVc);
-    //console.log(signedVc.toString);
+    console.log("Press 1 for sharing only name and birthdate");
+    console.log("Press 2 for sharing all data");
+    let choice = prompt("Your choise ");
+    if(choice == "1"){
+        
+        credentialSubject = {
+            id: citizen_id.doc.id.toString(),
+            first_name: "Ron",
+            birthdate:"08201999"
+        };
+
+        // Create an unsigned `Decentralized Identity` credential for citizen_id
+        const unsignedVc = VerifiableCredential.extend({
+            id: "http://groninen.edu/credentials/3732",
+            type: "NationalIdentityCredentials",
+            issuer: government_id.doc.id.toString(),
+            credentialSubject,
+        });
+        // Sign the credential with the government_id's newKey
+        const signedVc = government_id.doc.signCredential(unsignedVc, {
+            method: government_id.doc.id.toString()+"#newKey",
+            public: government_id.newKey.public,
+            secret: government_id.newKey.secret,
+        });
+
+        saveQR(JSON.stringify(signedVc));
+        console.log("Verifiable credentials is successfull..............");
+        
+
+    }else{
+        
+        // Create an unsigned `Decentralized Identity` credential for citizen_id
+        const unsignedVc = VerifiableCredential.extend({
+            id: "http://groninen.edu/credentials/3732",
+            type: "NationalIdentityCredentials",
+            issuer: government_id.doc.id.toString(),
+            credentialSubject,
+        });
+        // Sign the credential with the government_id's newKey
+        const signedVc = government_id.doc.signCredential(unsignedVc, {
+            method: government_id.doc.id.toString()+"#newKey",
+            public: government_id.newKey.public,
+            secret: government_id.newKey.secret,
+        });
+
+        saveQR(JSON.stringify(signedVc));
+        console.log("Verifiable credentials is successfull..............");
+    }
+    // uncomment to get the QR code
+
     //console.log("You can use this QR-code to show to your verifiable credentials");
     //console.log(
     //    `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${JSON.stringify(signedVc)}`);
-    
-    saveQR(JSON.stringify(signedVc));
+
 }
 
 run();
